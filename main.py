06e39720b5a664fad33f4da8225fd231351168a0
FilipMaksymiuk7 <filipex12045@wp.pl>
2023-03-08 15:51:24 +0100

    cw2python

---
+++ main.py
@@ -0,0 +1,87 @@
+# a = 'napis\ndrugi napis'
+# print(a)
+# b = 5
+# c = 5.5
+# print(b, c)
+# d = 5+3j
+# print(d)
+# f = c//b
+# print(f)
+# e = c+d
+# print(e)
+# g = c % b
+# print(g)
+# h = b**2
+# print(h)
+# j = pow(b,1/2)
+# print(j)
+#
+# print('b=b+2')
+# print(b)
+# b+=2
+# print(b)
+#
+
+# listy = ['a',4,5,6,[1,2,3],5.6]
+# print(listy)
+# listy.append(4)
+# print(listy)
+# print(listy[5])
+lista =[1,2,3,]
+lista2 =[4,5,6]
+#dodać element na wybrane miejsce
+lista.insert(2,0)
+print(lista)
+#dodac kilka elementow
+lista.extend(lista2)
+print(lista)
+#usunac element z listy o danej pozycji
+del lista[2]
+print(lista)
+#usunąć element z listy o danej wartości
+lista.remove(3)
+print(lista)
+#odwrócić element z listy
+lista.reverse()
+print(lista)
+#zrobić sortowanie
+lista.sort()
+print(lista)
+
+#tupla = (1,2,3,4,5)
+#dict = {1:'tak',0:'nie','2':moze}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
